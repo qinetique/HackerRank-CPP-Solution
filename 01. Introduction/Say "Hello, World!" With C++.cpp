31cd8b9bf31f785ac830396037f5c76afac45523
Tonmoy Mondal <dev.tonmoy@proton.me>
2023-07-27 09:07:44 +0530

    01

---
+++ 01. Introduction/Say "Hello, World!" With C++.cpp
@@ -0,0 +1,12 @@
+/*Author: Tonmoy M
+**URI: https://qinetique.github.io
+*/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
